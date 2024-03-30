baecbf11b881937f4154480e4ad2f8a57a3129de
rvanwijk <rickvanwijk@bluebirdday.nl>
2024-03-30 16:30:32 +0100

    init commit for todo app

---
+++ frontend-mentor-challenges/todo-app/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+ <home-view></home-view>
+</template>
+
+<script lang="ts" setup>
+import HomeView from './views/HomeView.vue'
+</script>
+<style lang="css">
+html, body {
+    width: 100%;
+    height: 100%;
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+}
+</style>
---
+++ frontend-mentor-challenges/todo-app/src/views/HomeView.vue
@@ -0,0 +1,38 @@
+<template>
+  <main>
+    <div>
+      <floating-button onclick="noteOptions.showModal()"></floating-button>
+      <dialog id="noteOptions">
+        <form method="dialog">
+          <p>Test</p>
+          <button onclick="noteOptions.closeModal()">ok</button>
+          <button @click="console">ok</button>
+        </form>
+      </dialog>
+    </div>
+  </main>
+</template>
+
+<script lang="ts" setup>
+import FloatingButton from '../components/FloatingButton.vue'
+import { ref } from 'vue';
+
+const console = () => {
+
+}
+
+// const isOpen = ref(false);
+// const openOptions = () => {
+//   isOpen.value = !isOpen.value;
+// }
+</script>
+
+<style lang="css">
+.overlay {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: transparent;
+  overflow: hidden;
+}
+</style>
