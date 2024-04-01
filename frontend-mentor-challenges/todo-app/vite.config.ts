bb42ab1c4e866dfcf16867fba67d090c040a7548
rvanwijk <rickvanwijk@bluebirdday.nl>
2024-04-01 21:41:05 +0200

    edited baseurl

--- frontend-mentor-challenges/todo-app/vite.config.ts
+++ frontend-mentor-challenges/todo-app/vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/frontend-challenges/frontend-mentor-challenges/todo-app/",
+  base: "/frontend-challenges/",
   plugins: [
     vue({
       script: {
