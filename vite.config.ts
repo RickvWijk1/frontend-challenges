2baba8c8d15b89c1aaa02a5050942ac2a7e05054
rvanwijk <rickvanwijk@bluebirdday.nl>
2024-04-02 23:05:52 +0200

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/frontend-challenges/',
+  base: 'https://rickvwijk1.github.io/frontend-challenges/',
   plugins: [
     vue({
       script: {
