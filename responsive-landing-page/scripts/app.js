4c5c48629c8ca487721d9012a5ccc4d46a839de4
rvanwijk <rickvanwijk12@gmail.com>
2025-03-09 16:49:42 +0100

    Updates

--- responsive-landing-page/scripts/app.js
+++ responsive-landing-page/scripts/app.js
@@ -1,10 +1,7 @@
-    let getHamburger = document.querySelectorAll('#toggle-bar');
-    getHamburger.forEach((toggleMenu) => {
-        toggleMenu.onclick = () => {
-            let getMenu = document.querySelector(".nav-mobile");
-            getMenu.classList.toggle("hamburger");
-        };
-    });
-    
-    
-
+let getHamburger = document.querySelectorAll('#toggle-bar')
+getHamburger.forEach((toggleMenu) => {
+  toggleMenu.onclick = () => {
+    let getMenu = document.querySelector('.nav-mobile')
+    getMenu.classList.toggle('hamburger')
+  }
+})
