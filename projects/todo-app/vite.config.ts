3961d039fb21b39e1413d355251fd576f188e9bd
rvanwijk <rickvanwijk12@gmail.com>
2025-03-09 21:20:34 +0100

    Fix Vue base path for GitHub Pages

--- projects/todo-app/vite.config.ts
+++ projects/todo-app/vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/todo-app',
+  base: '/frontend-challenges/todo-app',
   plugins: [
     vue({
       script: {
