76903b7d2c0af92ea51aaf5572110bfe03413002
rvanwijk <rickvanwijk12@gmail.com>
2025-03-09 20:33:33 +0100

    reroute the base path of the vue to-do app project

--- projects/todo-app/vite.config.ts
+++ projects/todo-app/vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/frontend-challenges/',
+  base: '/frontend-challenges/projects/todo-app/',
   plugins: [
     vue({
       script: {
