2b9b3c2c45a5c06d82d5e64d3b1d466b0dedbd27
rvanwijk <rickvanwijk12@gmail.com>
2025-03-09 17:00:47 +0100

    removed dead folders and files

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import TodoPage from '../pages/todo/TodoPage.vue'
-import LandingPage from '@/pages/responsive-landing-page/LandingPage.vue'
+// import LandingPage from '@/pages/responsive-landing-page/LandingPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,16 +15,16 @@ const router = createRouter({
       name: 'todo-app',
       component: TodoPage
     },
-    {
-      path: '/responsive-landing-page',
-      name: 'responsive-landing-page',
-      component: LandingPage
-    },
-    {
-      path: '/notification-page',
-      name: 'notification-page',
-      component: LandingPage
-    },
+    // {
+    //   path: '/responsive-landing-page',
+    //   name: 'responsive-landing-page',
+    //   component: LandingPage
+    // },
+    // {
+    //   path: '/notification-page',
+    //   name: 'notification-page',
+    //   component: LandingPage
+    // },
   ]
 })
 
